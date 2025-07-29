19d0798dab5a0b1e9f47c228652315e61d99a949
Daniil Efimow <daniil.efimow@mail.ru>
2025-07-29 22:01:44 +0300

    add schemas and negative test

--- tests/test_reqres_in.py
+++ tests/test_reqres_in.py
@@ -1,28 +1,36 @@
-from http.client import responses
-
 import requests
+from jsonschema import validate
+
+import schemas
 
 url = "https://reqres.in/"
-endpoint = '/api/users'
+list_user_endpoint = '/api/users'
+endpoint = '/api/users/'
 register_endpoint = '/api/register'
 login_endpoint = '/api/login'
 
 def test_list_users():
-    response = requests.get(url + endpoint, params={'page': 2})
+    response = requests.get(url + list_user_endpoint, params={'page': 2})
     body = response.json()
 
     assert response.status_code == 200
     assert body['page'] == 2
 
 
-def test_single_user():
-    single_user_endpoint = '/2'
-
-    response = requests.get(url + endpoint + single_user_endpoint)
+def test_get_single_user():
+    user_id = '2'
+    headers = {
+        'x-api-key': 'reqres-free-v1'
+    }
+    response = requests.get(url + endpoint + user_id, headers=headers)
     body = response.json()
 
     assert response.status_code == 200
-    assert body['data']["id"] == 2
+    assert body["data"]["email"] == "janet.weaver@reqres.in"
+    assert body["data"]["first_name"] == "Janet"
+    assert body["data"]["last_name"] == "Weaver"
+
+    validate(body, schema=schemas.get_single_user)
 
 def test_single_user_not_found():
     single_user_endpoint = '/23'
@@ -50,11 +58,12 @@ def test_create_user():
     response = requests.post(url + endpoint, json=payload, headers=headers)
 
     assert response.status_code == 201
+    validate(response.json(), schema=schemas.create_user)
 
 def test_update_user():
     name = 'daniil'
     job = 'qa'
-    id = '/2'
+    id = '2'
 
     payload = {
         'name': name,
@@ -68,9 +77,11 @@ def test_update_user():
     response = requests.put(url + endpoint + id, json=payload, headers=headers)
 
     assert response.status_code == 200
+    validate(response.json(), schema=schemas.update_user)
+
 
 def test_delete_user():
-    id = '/2'
+    id = '2'
 
     headers = {
         'x-api-key': 'reqres-free-v1'
@@ -94,9 +105,9 @@ def test_register_success():
     }
 
     response = requests.post(url + register_endpoint, json=payload, headers=headers)
-    body = response.json()
 
     assert response.status_code == 200
+    validate(response.json(), schema=schemas.register_user)
 
 def test_register_unsuccess():
     email = 'eve.holt@reqres.in'
@@ -113,6 +124,7 @@ def test_register_unsuccess():
     body = response.json()
 
     assert response.status_code == 400
+    validate(response.json(), schema=schemas.register_unsuccessful)
 
 def test_login_success():
     email = 'eve.holt@reqres.in'
@@ -137,6 +149,23 @@ def test_login_unsuccess():
 
     payload = {
         'email': email,
+    }
+
+    headers = {
+        'x-api-key': 'reqres-free-v1'
+    }
+
+    response = requests.post(url + register_endpoint, json=payload, headers=headers)
+
+    assert response.status_code == 400
+
+def test_login_with_negative_email():
+    email = 'eve.holtreqres.in'
+    password = 'pistol'
+
+    payload = {
+        'email': email,
+        'password': password
     }
 
     headers = {
